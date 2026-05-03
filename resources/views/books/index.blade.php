31398afc394628391d36a3ad1ff3be5d05fe0639
Miloslav Šmíd <97102231+M1l069@users.noreply.github.com>
2026-05-03 21:22:12 +0200

    added index() for books and filtering books based on the book`s title + updated language and added favicon

---
+++ resources/views/books/index.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.app')
+
+@section('content')
+
+<h1 class="mb-10 text-2xl">Books</h1>
+
+<form action="{{ route('books.index') }}" method="GET" class="mb-4 flex items-center space-x-2">
+    <input type="text" name="title" placeholder="Hľadať podľa názvu..." value="{{ request('title') }}" class="input h-10">
+    <button type="submit" class="btn h-10">Hľadať</button>
+    <a href="{{ route('books.index') }}" class="btn h-10">Vyčistiť</a>
+</form>
+<ul>
+    @forelse ($books as $book)
+        <li class="mb-4">
+            <div class="book-item">
+                <div
+                    class="flex flex-wrap items-center justify-between">
+                    <div class="w-full flex-grow sm:w-auto">
+                        <a href="{{ route('books.show', $book) }}" class="book-title"> {{ $book->title }} </a>
+                        <span class="book-author">od {{ $book->author }} </span>
+                    </div>
+                    <div>
+                        <div class="book-rating">
+                            {{ number_format($book->reviews_avg_rating, 1) }}
+                        </div>
+                        <div class="book-review-count">
+                            z {{ $book->reviews_count }} {{ Str::plural('review', $book->reviews_count) }}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </li>
+    @empty
+        <li class="mb-4">
+            <div class="empty-book-item">
+                <p class="empty-text">Nenašli sa žiadne knihy</p>
+                <a href="{{ route('books.index') }}" class="reset-link">Resetovať kritérium</a>
+            </div>
+        </li>
+    @endforelse
+</ul>
+@endsection
