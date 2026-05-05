3a020b87facacfdb03eead8560461ab074eeeb78
Miloslav Šmíd <97102231+M1l069@users.noreply.github.com>
2026-05-05 02:18:54 +0200

    upravený dizajn books.reviews.create.blade.php a aj pre menšie zariadenia

--- resources/views/books/reviews/create.blade.php
+++ resources/views/books/reviews/create.blade.php
@@ -1,10 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1 class="mb-10 text-2xl">Pridať recenziu pre {{ $book->title }}</h1>
-
-    <form method="POST" action="{{ route('books.reviews.store', $book) }}">
+    <form method="POST" action="{{ route('books.reviews.store', $book) }}" class="mx-4 flex max-w-lg flex-col gap-4 rounded-lg border border-slate-200 bg-white p-6 shadow-sm min-[544px]:mx-auto">
         @csrf
+        <h1 class="mb-10 text-2xl text-center">Pridať recenziu pre {{ $book->title }}</h1>
         <label for="review">Recenzia: </label>
         <textarea name="review" id="review" required class="input mb-4"></textarea>
         <label for="rating">Hodnotenie: </label>
@@ -15,5 +14,6 @@
             @endfor
         </select>
         <button type="submit" class="btn">Pridať recenziu</button>
+        <a href="{{ route('books.show', $book) }}" class="btn">Naspäť</a>
     </form>
 @endsection
